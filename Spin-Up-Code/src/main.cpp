2f8dc69dc8e98bb31d918084fb7769fa3865dbfc
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2023-02-03 16:35:30 -0800

    possibly TBH code?

--- Spin-Up-Code/src/main.cpp
+++ Spin-Up-Code/src/main.cpp
@@ -257,7 +257,7 @@ void turnPID(int desiredValue, double multiplier){
 
 
 // FLYWHEEL PID
-void flywheelPID(){ // using rpm of the flywheel as "distance"
+int flywheelPID(){ // using rpm of the flywheel as "distance"
   int desiredRpm = 430;
   //Settings - variables initializations
   double kP = 0.05;
@@ -295,6 +295,7 @@ void flywheelPID(){ // using rpm of the flywheel as "distance"
     
     prevError = error;
   
+    vex::task::sleep(15);
   
     // if (abs(error) < stopPID) {
     //   Brain.Screen.print(abs(error));
@@ -304,6 +305,58 @@ void flywheelPID(){ // using rpm of the flywheel as "distance"
 
   //stop the wheels after the while loop is done
   // FlywheelMotor
+  return 0;
+}
+
+// e = S-P;                            // calculate the error;
+// Y += G*e;                           // integrate the output;
+// if (Y>1) Y=1; else if (Y<0) Y=0;    // clamp the output to 0..+1;
+// if (signbit(e)!=signbit(d)){        // if zero crossing,
+//   Y = b = 0.5*(Y+b);                // then Take Back Half
+//   d = e;}                           // and save the previous error;
+
+// S is the setpoint (target RPM)
+// P is the process variable (measured RPM)
+// G is the integral gain (the tuning parameter)
+// Y is the output command to the motor controller
+// e is the error
+// d is the previous error
+// b is the TBH variable
+
+// Y, d, and b should be initialized.
+
+int TBH() {
+  // Define Constants
+  double output = 0; // output value to the motor
+  double tbh = 0; // tbh tuning constant
+  double pI = 0; // integral tuning constant
+
+  double prevError = 0; // previous error
+  double targetRpm = 430; // target flywheel RPM
+
+  // Calculate the error: target RPM - calculated RPM
+  double error = targetRpm - FlywheelMotor.velocity(rpm);
+
+  // Integrate the output: Integral Tuning Variable * error
+  output += pI*error;
+
+  // clamp the output
+  if (output > 1) {
+    output = 1;
+  } else if (output < 0) {
+    output = 0;
+  }
+
+  // if the error has changed sign since the last loop
+  if (signbit(error) != signbit(prevError)) {
+    output = tbh = 0.5*(output + tbh);
+    prevError = error;
+  }
+
+  // spin the flywheel motor
+  FlywheelMotor.spin(reverse, output, volt);
+
+  return 0;
 }
 
 
@@ -947,9 +1000,8 @@ void usercontrol(void) {
       else speed = 1;
     }
 
-    task gammas(flywheelPID());
-
-    flywheelPID(430);
+    vex::task t1(flywheelPID);
+    // flywheelPID();
 
     wait(20, msec); // Sleep the task for a short amount of time to prevent wasted resources
   }
